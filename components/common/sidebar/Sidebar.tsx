0572aeb66aed7b767b82567be5a14e40410580db
DDD120 <dltnals9794@naver.com>
2023-11-19 14:57:02 +0900

    feat user menu in sidebar

--- components/common/sidebar/Sidebar.tsx
+++ components/common/sidebar/Sidebar.tsx
@@ -1,6 +1,22 @@
 'use client'
 
-import { Box, Button, Flex, List, Show, theme } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Hide,
+  List,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Portal,
+  Show,
+  Text,
+  theme,
+} from '@chakra-ui/react'
 import SidebarLink from './SidebarLink'
 import {
   PiHouseDuotone as HomeIcon,
@@ -12,52 +28,83 @@ import {
   PiUserDuotone as ProfileIcon,
   PiDotsThreeOutlineDuotone as MoreIcon,
   PiBirdDuotone as BirdIcon,
+  PiDotsThreeOutlineVerticalDuotone as DotsIcon,
 } from 'react-icons/pi'
+import { useSession } from 'next-auth/react'
 
 function Sidebar() {
+  const { data: session } = useSession()
   return (
     <Show above='sm'>
       <Flex
         flexDir='column'
-        alignItems={{ md: 'cneter', lg: 'flex-start' }}
+        alignItems={{ sm: 'cneter', lg: 'flex-start' }}
+        justifyContent='space-between'
         h='full'
         position='fixed'
         py={4}
         pr={4}
       >
-        <Box
-          w='32px'
-          h='32px'
-          p={4}
-          borderRadius='full'
-          _hover={{
-            cursor: 'pointer',
-          }}
-        >
-          <BirdIcon size={32} />
-        </Box>
-        <List spacing={2} mt={8} mb={4}>
-          <SidebarLink text='홈' Icon={HomeIcon} active />
-          <SidebarLink text='탐색하기' Icon={ExploreIcon} />
-          <SidebarLink text='알림' Icon={NotificationsIcon} />
-          <SidebarLink text='메시지' Icon={MessagesIcon} />
-          <SidebarLink text='북마크' Icon={BookmarksIcon} />
-          <SidebarLink text='리스트' Icon={ListsIcon} />
-          <SidebarLink text='프로필' Icon={ProfileIcon} />
-          <SidebarLink text='더보기' Icon={MoreIcon} />
-        </List>
-        <Show above='lg'>
-          <Button
-            w='224px'
-            h='52px'
-            flexShrink={0}
-            rounded='full'
-            backgroundColor={theme.colors.blackAlpha[200]}
-            fontSize='lg'
+        <Box w='full'>
+          <Box
+            w='32px'
+            h='32px'
+            p={4}
+            borderRadius='full'
+            _hover={{
+              cursor: 'pointer',
+            }}
           >
-            Peep
-          </Button>
-        </Show>
+            <BirdIcon size={32} />
+          </Box>
+          <List spacing={2} mt={8} mb={4}>
+            <SidebarLink text='홈' Icon={HomeIcon} active />
+            <SidebarLink text='탐색하기' Icon={ExploreIcon} />
+            <SidebarLink text='알림' Icon={NotificationsIcon} />
+            <SidebarLink text='메시지' Icon={MessagesIcon} />
+            <SidebarLink text='북마크' Icon={BookmarksIcon} />
+            <SidebarLink text='리스트' Icon={ListsIcon} />
+            <SidebarLink text='프로필' Icon={ProfileIcon} />
+            <SidebarLink text='더보기' Icon={MoreIcon} />
+          </List>
+          <Show above='lg'>
+            <Button
+              w='full'
+              h='52px'
+              flexShrink={0}
+              rounded='full'
+              backgroundColor={theme.colors.blackAlpha[200]}
+              fontSize='lg'
+            >
+              Peep
+            </Button>
+          </Show>
+        </Box>
+        <Menu>
+          <MenuButton h={16} rounded='full' variant='ghost' as={Button}>
+            <Flex gap={{ md: 2 }} alignItems='center'>
+              <Avatar
+                size='sm'
+                name={session?.user.name!}
+                src={session?.user.image!}
+              />
+              <Show above='lg'>
+                <Box textAlign='left'>
+                  <Heading as='h3' size='sm'>
+                    {session?.user.name}
+                  </Heading>
+                  <Text fontSize='xs'>@{session?.user.tag}</Text>
+                </Box>
+                <DotsIcon />
+              </Show>
+            </Flex>
+          </MenuButton>
+          <Portal>
+            <MenuList>
+              <MenuItem>로그아웃</MenuItem>
+            </MenuList>
+          </Portal>
+        </Menu>
       </Flex>
     </Show>
   )
