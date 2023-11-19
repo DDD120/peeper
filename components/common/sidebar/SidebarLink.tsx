457efbf8870053c58ded3714dad9f6f269c322fb
DDD120 <dltnals9794@naver.com>
2023-11-19 14:58:22 +0900

    style sidebar link

--- components/common/sidebar/SidebarLink.tsx
+++ components/common/sidebar/SidebarLink.tsx
@@ -1,5 +1,5 @@
-import { ListIcon, ListItem, Show, theme } from "@chakra-ui/react"
-import { IconType } from "react-icons/lib/esm/iconBase"
+import { Flex, ListIcon, ListItem, Show, theme } from '@chakra-ui/react'
+import { IconType } from 'react-icons/lib/esm/iconBase'
 
 interface Props {
   text: string
@@ -9,24 +9,28 @@ interface Props {
 
 function SidebarLink({ text, Icon, active }: Props) {
   return (
-    <ListItem
-      display="flex"
-      gap={2}
-      alignItems="center"
-      fontWeight={active ? 700 : 400}
-      transition={"all 0.3s"}
-      padding={2}
-      cursor="pointer"
-      borderRadius="4px"
-      _hover={{
-        backgroundColor: theme.colors.gray[200],
-      }}
-      _active={{
-        backgroundColor: theme.colors.gray[300],
-      }}
-    >
-      <ListIcon as={Icon} w={8} h={8} mr={0} />
-      <Show above="lg">{text}</Show>
+    <ListItem>
+      <Flex
+        display='inline-flex'
+        gap={2}
+        justifyContent={{ sm: 'center', lg: 'flex-start' }}
+        alignItems='center'
+        fontWeight={active ? 700 : 400}
+        transition={'all 0.3s'}
+        py={2}
+        px={4}
+        cursor='pointer'
+        borderRadius='full'
+        _hover={{
+          backgroundColor: theme.colors.gray[100],
+        }}
+        _active={{
+          backgroundColor: theme.colors.gray[200],
+        }}
+      >
+        <ListIcon as={Icon} w={8} h={8} mr={0} />
+        <Show above='lg'>{text}</Show>
+      </Flex>
     </ListItem>
   )
 }
